c6f01f7079539d796116f27e040be16148c200c2
prabhasyadav <prabhasyadav@gmail.com>
2021-02-28 22:01:43 +0100

    Update documentation

---
+++ _sources/contents/flow/15_aq_hetero.m
@@ -0,0 +1,12 @@
+# Lecture 5- Aquifer heterogeneity 
+
+## Prof. Liedl/Prof. Werth/Prabhas is to add the text contents.
+## Anne/Sophie is to add the numerical contents.
+
+
+## layered system
+
+Include the ipynb for eff conductive.
+
+
+
